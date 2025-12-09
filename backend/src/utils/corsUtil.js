f5a81241be5b2c0603216f8523dfe65fa3e7e19c
bhaveshjadhav <bhaveshjadhav@mobiliya.io>
2025-12-09 15:48:45 +0530

    added new deployed url in backend cors

--- backend/src/utils/corsUtil.js
+++ backend/src/utils/corsUtil.js
@@ -3,6 +3,8 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:8080",  
   "http://127.0.0.1:8080",
+  "https://auto-rfp-ai.vercel.app",
+  "auto-rfp-ai.vercel.app"
 ];
 
 module.exports = { allowedOrigins };
